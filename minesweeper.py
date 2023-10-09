1cba77d41212afb07139a6e1bc6fe6b3c967f0bd
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-10-09 09:40:23 -0500

    Update minesweeper.py
        initialize_board() now returns a tuple containing both the board and the set of mine positions.
        print_board() has been updated to accept the mines set as an argument.
        Updated count_mines() to use the mines set.
        reveal_cells() now takes mines set as an argument.
        In the main() function, we directly check for the presence of a mine using the set, rather than checking the board content.

--- minesweeper.py
+++ minesweeper.py
@@ -2,56 +2,57 @@ import random
 
 def initialize_board(size, num_mines):
     board = [[' ' for _ in range(size)] for _ in range(size)]
-    mines_placed = 0
-    while mines_placed < num_mines:
+    mines = set()
+    while len(mines) < num_mines:
         row = random.randint(0, size - 1)
         col = random.randint(0, size - 1)
-        if board[row][col] != 'M':
+        if (row, col) not in mines:
+            mines.add((row, col))
             board[row][col] = 'M'
-            mines_placed += 1
-    return board
+    return board, mines
 
-def print_board(board, reveal=False):
-    for row in board:
-        for cell in row:
-            if cell == 'M' and reveal:
+def print_board(board, mines, reveal=False):
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if (row, col) in mines and reveal:
                 print('M', end=' ')
-            elif cell == 'M' or cell == 'F':
+            elif board[row][col] == 'F':
+                print('F', end=' ')
+            elif (row, col) in mines:
                 print('.', end=' ')
             else:
-                print(cell, end=' ')
+                print(board[row][col], end=' ')
         print()
 
-def count_mines(board, row, col):
+def count_mines(mines, row, col):
     count = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
             r, c = row + i, col + j
-            if 0 <= r < len(board) and 0 <= c < len(board[0]):
-                if board[r][c] == 'M':
-                    count += 1
+            if (r, c) in mines:
+                count += 1
     return count
 
-def reveal_cells(board, row, col):
+def reveal_cells(board, mines, row, col):
     if board[row][col] != ' ':
         return
-    mine_count = count_mines(board, row, col)
+    mine_count = count_mines(mines, row, col)
     if mine_count == 0:
         board[row][col] = ' '
         for i in range(-1, 2):
             for j in range(-1, 2):
                 r, c = row + i, col + j
                 if 0 <= r < len(board) and 0 <= c < len(board[0]):
-                    reveal_cells(board, r, c)
+                    reveal_cells(board, mines, r, c)
     else:
         board[row][col] = str(mine_count)
 
 def main():
     size = int(input("Enter board size: "))
     num_mines = int(input("Enter number of mines: "))
-    board = initialize_board(size, num_mines)
+    board, mines = initialize_board(size, num_mines)
     while True:
-        print_board(board)
+        print_board(board, mines)
         action = input("Choose action (reveal/flag): ").strip().lower()
         row, col = map(int, input("Enter row and column separated by space (e.g., 3 4): ").split())
         if action == 'flag':
@@ -60,15 +61,15 @@ def main():
             elif board[row][col] == 'F':
                 board[row][col] = ' '
             continue
-        if board[row][col] == 'M':
+        if (row, col) in mines:
             print("You hit a mine! Game over.")
-            print_board(board, reveal=True)
+            print_board(board, mines, reveal=True)
             break
         else:
-            reveal_cells(board, row, col)
+            reveal_cells(board, mines, row, col)
             if all(cell == 'M' or cell != ' ' for row in board for cell in row):
                 print("Congratulations! You've revealed all safe cells.")
-                print_board(board, reveal=True)
+                print_board(board, mines, reveal=True)
                 break
 
 if __name__ == '__main__':
