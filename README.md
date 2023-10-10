6712f5631ae4573dfb0cad11b1b7815e3f94eccb
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-10-10 10:45:39 -0500

    Whitespace, readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# minesweeper
- 
+A simple command-line implementation of the classic Minesweeper game. In this game, you need to reveal cells without hitting a mine. You can also flag cells that you believe contain mines.
+Features
+
+    Interactive grid with row and column numbers for easy cell selection.
+    Option to either reveal a cell or flag it.
+    Automatically reveals cells with no adjacent mines and provides count of adjacent mines when a cell is revealed.
+    Win condition check, to determine if all safe cells have been revealed.
+
+Usage
+
+Run the script to play the game:
+python minesweeper.py
+
+Game Flow:
+
+    Input the board size.
+    Input the number of mines.
+    Choose an action:
+        reveal to reveal a cell.
+        flag to flag or unflag a cell.
+    Enter row and column number separated by a space to select a cell (e.g., 3 4).
+    The game continues until either all safe cells are revealed or a mine is hit.
+
+Functions
+
+    initializeBoard(size, numMines): Generates a board of given size with the specified number of mines.
+    printBoard(board, mines, reveal=False): Prints the game board. If reveal is set to True, it will show mines.
+    countMines(mines, row, col): Counts the number of mines adjacent to a given cell.
+    revealCells(board, mines, row, col): Reveals cells recursively if no adjacent mines are present.
+    hasWon(board, mines): Checks if all safe cells have been revealed to determine win condition.
+    main(): The main game loop where user interactions take place.
--- minesweeper.py
+++ minesweeper.py
@@ -1,26 +1,33 @@
 import random
 
-def initialize_board(size, num_mines):
+def initializeBoard(size, numMines):
     board = [[' ' for _ in range(size)] for _ in range(size)]
     mines = set()
-    while len(mines) < num_mines:
+
+    while len(mines) < numMines:
         row = random.randint(0, size - 1)
         col = random.randint(0, size - 1)
+
         if (row, col) not in mines:
             mines.add((row, col))
             board[row][col] = 'M'
+
     return board, mines
 
-def print_board(board, mines, reveal=False):
+def printBoard(board, mines, reveal=False):
     size = len(board)
+
     # Print column headers
     print("   ", end='')
-    for col_num in range(size):
-        print(f"{col_num} ", end='')
+
+    for colNum in range(size):
+        print(f"{colNum} ", end='')
+
     print("\n" + "  " + "-" * (size * 2))
 
     for row in range(size):
         print(f"{row}|", end=' ')
+
         for col in range(size):
             if (row, col) in mines and reveal:
                 print('M', end=' ')
@@ -30,55 +37,65 @@ def print_board(board, mines, reveal=False):
                 print('.', end=' ')
             else:
                 print(board[row][col], end=' ')
+
         print()
 
-def count_mines(mines, row, col):
+def countMines(mines, row, col):
     count = 0
+
     for i in range(-1, 2):
         for j in range(-1, 2):
             r, c = row + i, col + j
             if (r, c) in mines:
                 count += 1
+
     return count
 
-def reveal_cells(board, mines, row, col):
+def revealCells(board, mines, row, col):
     if board[row][col] != ' ':
         return
-    mine_count = count_mines(mines, row, col)
-    if mine_count == 0:
+    
+    mineCount = countMines(mines, row, col)
+
+    if mineCount == 0:
         board[row][col] = '0'  # Mark the cell as a '0' cell
+
         for i in range(-1, 2):
             for j in range(-1, 2):
                 r, c = row + i, col + j
                 if 0 <= r < len(board) and 0 <= c < len(board[0]) and board[r][c] == ' ':
-                    reveal_cells(board, mines, r, c)
+                    revealCells(board, mines, r, c)
+
     else:
-        board[row][col] = str(mine_count)
+        board[row][col] = str(mineCount)
 
 
-def has_won(board, mines):
+def hasWon(board, mines):
     for row in range(len(board)):
         for col in range(len(board[0])):
             if (row, col) not in mines and board[row][col] == ' ':
                 return False
+            
     return True
 
 def main():
     size = int(input("Enter board size: "))
-    num_mines = int(input("Enter number of mines: "))
-    board, mines = initialize_board(size, num_mines)
+    numMines = int(input("Enter number of mines: "))
+    board, mines = initializeBoard(size, numMines)
     # Create the list of numbers outside the while loop
-    already_revealed = ['F'] + [str(x) for x in range(1, 9)]
+    alreadyRevealed = ['F'] + [str(x) for x in range(1, 9)]
     
     while True:
-        print_board(board, mines)
+        printBoard(board, mines)
         action = input("Choose action (reveal/flag): ").strip().lower()
+
         while action not in ['reveal', 'flag']:
             print("Invalid action. Please choose either 'reveal' or 'flag'.")
             action = input("Choose action (reveal/flag): ").strip().lower()
+
         row, col = map(int, input("Enter row and column separated by space (e.g., 3 4): ").split())
         
-        while row < 0 or row >= size or col < 0 or col >= size or board[row][col] in already_revealed:
+        while row < 0 or row >= size or col < 0 or col >= size or board[row][col] in alreadyRevealed:
             print("Invalid input. Please enter a valid row and column.")
             row, col = map(int, input("Enter row and column separated by space (e.g., 3 4): ").split())
 
@@ -91,13 +108,15 @@ def main():
         
         if (row, col) in mines:
             print("You hit a mine! Game over.")
-            print_board(board, mines, reveal=True)
+            printBoard(board, mines, reveal=True)
             break
+
         else:
-            reveal_cells(board, mines, row, col)
-            if has_won(board, mines):
+            revealCells(board, mines, row, col)
+            
+            if hasWon(board, mines):
                 print("Congratulations! You've revealed all safe cells.")
-                print_board(board, mines, reveal=True)
+                printBoard(board, mines, reveal=True)
                 break
 
 if __name__ == '__main__':
